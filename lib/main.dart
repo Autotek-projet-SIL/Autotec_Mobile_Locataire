8c8d40ac5e300e003fcad55db909470a0b6ae83a
akli hiba <ih_akli@esi.dz>
2022-04-13 21:42:11 +0100

    notifications configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Authentication/bloc/bloc/auth_bloc.dart';
 import 'Authentication/data/repositories/auth_repository.dart';
 import 'Authentication/presentation/first_screens/intro.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 var userCred ;
+
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   userCred = userCredentials();
   await Firebase.initializeApp();
+
+  //Get permission for notifications
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 
 }
